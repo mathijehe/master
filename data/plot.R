3e6aaf7c98159b1231a9b39c7848bcc6ec16c72f
Mathias Hetland <din@email.commathias.hetland@hotmail.com>
2025-01-20 15:33:04 +0100

    k

--- data/plot.R
+++ data/plot.R
@@ -1,6 +1,8 @@
 data <- readRDS("data.rds")
 
 library(tidyverse)
+library(ggthemes)
+library(broom)
 mean(data$BNP, na.rm = TRUE)  
 
 
@@ -23,11 +25,30 @@ data %>%
   labs(title = "Gjennomsnittlig BNP etter statsminister",
        x = NULL,  # Fjerner tekst på x-aksen
        y = NULL) + # Fjerner tekst på y-aksen
-  theme_bw() +
+  theme_few() +
   scale_fill_manual(values = c("Høyre" = "blue2", "Venstre" = "red3")) +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1)) +  # Roterer teksten 45 grader
   scale_y_continuous(breaks = c(1,2,3,4,5))
 
 
+ll <- data %>%
+  slice(1:(n() - 3)) %>% 
+  slice(4:n()) %>% 
+  group_by(Regjering) %>%
+  summarise(
+    avg_gdp_growth = mean(BNP),
+    sd_gdp_growth = sd(BNP),
+    avg_unemployment = mean(arbeidsledighet),
+    sd_unemployment = sd(arbeidsledighet)
+  )
+
+t_test_gdp <- t.test(BNP ~ Regjering, data = data)
+t_test_unemployment <- t.test(arbeidsledighet ~ Regjering, data = data)
+
+# Vis resultatene
+results <- bind_rows(
+  tidy(t_test_gdp) %>% mutate(variable = "BNP"),
+  tidy(t_test_unemployment) %>% mutate(variable = "Arbeidsledighet")
+)
 
